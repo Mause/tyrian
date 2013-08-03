fc2a35f400841a604fab336dd3a3a350bc69471a
Dominic May <jack.thatch@gmail.com>
2013-08-03 22:34:26 +0800

    Rename to reflect content

--- src/lisp_runtime/defs.py
+++ src/lisp_runtime/defs.py
@@ -1,12 +1,6 @@
 from .registry import lisp_function
 
 
-@lisp_function(name="def-fun")
-def def_fun(*args, **kwargs):
-    "allows you to define a new function"
-    raise NotImplementedError()
-
-
 @lisp_function(name="defparameter")
 def defparameter(*args, **kwargs):
     """
