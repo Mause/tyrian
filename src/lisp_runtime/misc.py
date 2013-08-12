aa075266ba58c4ab89229b6b93dc887a50d3cc68
Dominic May <jack.thatch@gmail.com>
2013-08-12 17:51:31 +0800

    Added callfunc function helper function

--- src/lisp_runtime/misc.py
+++ src/lisp_runtime/misc.py
@@ -8,3 +8,11 @@ def return_func(arg):
     its output is used as the return value for the function
     """
     return arg
+
+
+@lisp_function(name="callfunc")
+def call_function(func, *args):
+    """
+    helper for calling function, usually lambda functions
+    """
+    return func(*args)
