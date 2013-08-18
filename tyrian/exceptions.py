b07f10a5971c4ae39c11319bf5f4d2d003a6f655
Dominic May <jack.thatch@gmail.com>
2013-08-18 16:32:44 +0800

    Exceptional documentation xD

--- tyrian/exceptions.py
+++ tyrian/exceptions.py
@@ -1,20 +1,36 @@
 
 
 class TyrianException(Exception):
+    """
+    Base exception to allow for easy catching of all exceptions raised by
+    tyrian
+    """
     pass
 
 
 class TyrianSyntaxError(TyrianException):
+    """
+    Raised when a syntax is found
+    """
     pass
 
 
 class InvalidToken(TyrianException):
+    """
+    Raised when an invalid token is found
+    """
     pass
 
 
 class GrammarDefinitionError(TyrianException):
+    """
+    Raised when the grammar definition file is found to have an error
+    """
     pass
 
 
 class NoSuchGrammar(TyrianException):
+    """
+    Raised when a reference grammar does not exist
+    """
     pass
