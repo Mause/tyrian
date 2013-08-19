16ed661a406f73320867bbbcf01fa79ca0f4e514
Dominic May <jack.thatch@gmail.com>
2013-08-20 07:11:15 +0800

    A fools errand?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,10 +24,16 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 cur_dir = os.path.abspath(os.path.dirname(__file__))
-raise Exception((cur_dir, os.getcwd()))
 sys.path.insert(0, os.path.abspath(os.path.join(cur_dir, '..\..')))
 sys.path.insert(0, os.path.abspath(os.path.join(cur_dir, '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(cur_dir, '.')))
+print((cur_dir, os.getcwd()))
+print(sys.path)
+try:
+    import tyrian
+    tyrian
+except ImportError as e:
+    print('Unable:', e)
 
 # -- General configuration -----------------------------------------------------
 
