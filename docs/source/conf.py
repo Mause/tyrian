89f06a33688aee03f9b3e691189f14593d9f4c66
Dominic May <jack.thatch@gmail.com>
2013-08-20 07:12:41 +0800

    Should i be prepared to be annoyed?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 cur_dir = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.abspath(os.path.join(cur_dir, '..\..')))
+sys.path.insert(0, os.path.abspath(os.path.join(cur_dir, '..', '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(cur_dir, '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(cur_dir, '.')))
 print((cur_dir, os.getcwd()))
