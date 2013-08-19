ad589d580370748179290e517ab8faec7b7c2992
Dominic May <jack.thatch@gmail.com>
2013-08-19 22:33:51 +0800

    Added base setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+import io
+import os
+from setuptools import setup
+
+import tyrian
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+
+setup(
+    name='tyrian',
+    version=tyrian.__version__,
+    url='http://github.com/Mause/tyrian/',
+    license='Apache Software License',
+    author='Dominic May',
+    # tests_require=['pytest'],
+    install_requires=[],
+    # cmdclass={'test': PyTest},
+    author_email='me@mause.me',
+    # description='Automated REST APIs for existing database-driven systems',
+    long_description=long_description,
+    packages=['tyrian'],
+    include_package_data=True,
+    platforms='any',
+    # test_suite='sandman.test.test_sandman',
+    classifiers=[
+        'Programming Language :: Python 3',
+        # 'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        # 'Environment :: Web Environment',
+        # 'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        # 'Topic :: Software Development :: Libraries :: Python Modules',
+        # 'Topic :: Software Development :: Libraries :: Application Frameworks',
+        # 'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+    extras_require={
+        # 'testing': ['pytest'],
+    }
+)
--- tyrian/__init__.py
+++ tyrian/__init__.py
@@ -2,3 +2,4 @@
 Tyrian, lisp to python bytecode
 """
 __package__ = 'tyrian'
+__version__ = 0.1
