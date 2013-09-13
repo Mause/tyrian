9c6dc9f5978c4a7fdb9f8584dd171b67066b163a
Dominic May <jack.thatch@gmail.com>
2013-09-13 23:05:46 +0800

    Assets!

---
+++ docs/source/assets/trace_table.py
@@ -0,0 +1,33 @@
+loaded_grammars = {}
+handle_setting = lambda: None
+clean = lambda x: x.strip()
+rules = '''\
+word ::= words words words;
+words ::= "blah" "blah" "blah";
+'''
+##### code that we a tracing follows
+
+
+rules = rules.replace("\n", " ")
+rules = rules.split(";")
+rules = map(str.rstrip, rules)
+rules = list(filter(bool, rules))
+
+while rules:
+    rule = rules.pop(0)
+
+    if rule.startswith("%"):
+        handle_setting()
+    elif rule.startswith("//"):
+        continue
+    else:
+        value = rule.split("::=")
+        key = value.pop(0)
+
+        key = key.upper().strip()
+
+        value = "::=".join(value)
+
+        value = clean(value)
+
+        loaded_grammars[key] = value
