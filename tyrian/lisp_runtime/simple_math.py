d85aa01658328000eb8b2c05467449b0e0750b66
Dominic May <jack.thatch@gmail.com>
2014-01-12 19:24:58 +0800

    Mended math builtins

--- tyrian/lisp_runtime/simple_math.py
+++ tyrian/lisp_runtime/simple_math.py
@@ -1,7 +1,3 @@
-# standard library
-from functools import reduce
-from operator import mul, truediv, sub
-
 # application specific
 from .registry import lisp_function
 
@@ -13,14 +9,29 @@ def symbol_simple_add(*args):
 
 @lisp_function(name="*")
 def symbol_simple_mul(*args):
+    from functools import reduce
+    from operator import mul
+
     return reduce(mul, args)
 
 
 @lisp_function(name="/")
 def symbol_simple_div(*args):
+    from functools import reduce
+    from operator import truediv
+
     return reduce(truediv, args)
 
 
 @lisp_function(name="-")
 def symbol_simple_sub(*args):
+    from functools import reduce
+    from operator import sub
+
     return reduce(sub, args)
+
+
+@lisp_function(name='sqrt')
+def sqrt(arg):
+    from math import sqrt as math_sqrt
+    return math_sqrt(arg)
